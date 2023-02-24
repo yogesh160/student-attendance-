fdee44f781586d342f82e1e723c0e4c2e3052f97
yogesh160 <94966283+yogesh160@users.noreply.github.com>
2023-02-24 11:15:09 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import InputBox from "./components/InputBox/InputBox";
+import StudentTable from "./components/StudentTable/StudentTable";
+
+function App() {
+  return (
+    <>
+      <InputBox />
+      <StudentTable />
+    </>
+  );
+}
+
+export default App;
